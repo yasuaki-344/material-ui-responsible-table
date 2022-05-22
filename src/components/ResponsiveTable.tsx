18b7e773b1c0999ef09c81dda2cf1a49234e1616
yasuaki-miyoshi <ikausay.443@gmail.com>
2022-05-22 16:17:49 +0900

    Make header area responsive

--- src/components/ResponsiveTable.tsx
+++ src/components/ResponsiveTable.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import TableCell from "@mui/material/TableCell";
 import Paper from "@mui/material/Paper";
 import { rows } from "./TableData";
 import { Box, Grid, Typography } from "@mui/material";
@@ -7,22 +6,30 @@ import { Box, Grid, Typography } from "@mui/material";
 export const ResponsiveTable = () => {
   return (
     <Box component={Paper}>
+      <Box
+        sx={{
+          display: { xs: "none", sm: "block" },
+        }}
+      >
+        <Grid container margin={2}>
+          <Grid item sm={4}>
+            <Typography variant="body2">Dessert (100g serving)</Typography>
+          </Grid>
+          <Grid item sm={2}>
+            <Typography variant="body2">Calories</Typography>
+          </Grid>
+          <Grid item sm={2}>
+            <Typography variant="body2">Fat&nbsp;(g)</Typography>
+          </Grid>
+          <Grid item sm={2}>
+            <Typography variant="body2">Carbs&nbsp;(g)</Typography>
+          </Grid>
+          <Grid item sm={2}>
+            <Typography variant="body2">Protein&nbsp;(g)</Typography>
+          </Grid>
+        </Grid>
+      </Box>
       <Grid container margin={2}>
-        <Grid item xs={4}>
-          <Typography variant="body2">Dessert (100g serving)</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography variant="body2">Calories</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography variant="body2">Fat&nbsp;(g)</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography variant="body2">Carbs&nbsp;(g)</Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography variant="body2">Protein&nbsp;(g)</Typography>
-        </Grid>
         {rows.map((row) => (
           <React.Fragment key={row.name}>
             <Grid item xs={4}>
