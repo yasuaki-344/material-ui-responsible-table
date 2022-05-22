6db69855f3c9bd4ad792e1073a80961fb5313aa2
yasuaki-344 <ikausay.443@gmail.com>
2022-05-22 14:38:18 +0900

    Add ResponsiveTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Grid } from "@mui/material";
 import React from "react";
 import "./App.css";
-import { BasicTable } from "./components";
+import { BasicTable, ResponsiveTable } from "./components";
 
 function App() {
   return (
     <Grid container spacing={1} margin={1}>
       <Grid item xs={12}>
         <BasicTable />
+      </Grid>
+      <Grid item xs={12}>
+        <ResponsiveTable />
       </Grid>
     </Grid>
   );
