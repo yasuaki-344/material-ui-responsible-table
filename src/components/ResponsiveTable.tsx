3a6f790e4e15047196e22f0ba48a9030b9ddad23
yasuaki-344 <ikausay.443@gmail.com>
2022-05-22 14:46:37 +0900

    Modify header design

--- src/components/ResponsiveTable.tsx
+++ src/components/ResponsiveTable.tsx
@@ -2,25 +2,32 @@ import * as React from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { rows } from "./TableData";
+import { Box, Grid, Typography } from "@mui/material";
 
 export const ResponsiveTable = () => {
   return (
-    <TableContainer component={Paper}>
+    <Box component={Paper}>
+      <Grid container margin={2}>
+        <Grid item xs={2}>
+          <Typography variant="body2">Dessert (100g serving)</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="body2">Calories</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="body2">Fat&nbsp;(g)</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="body2">Carbs&nbsp;(g)</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="body2">Protein&nbsp;(g)</Typography>
+        </Grid>
+      </Grid>
       <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
@@ -35,6 +42,6 @@ export const ResponsiveTable = () => {
           ))}
         </TableBody>
       </Table>
-    </TableContainer>
+    </Box>
   );
 };
